29f9f42b1bbf9366ec35828da4d19b719ed7b56f
Alison Stuart <alisonnicolestuart@gmail.com>
2016-12-27 10:03:25 -0500

    continuing the videos, trying to get the npm set up complete so I can properly debug

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,13 +2,21 @@ import React from 'react';
 import {getFunName} from '../helpers';
 
 class StorePicker extends React.Component {
+    goToStore(){
+        //first grab the text from teh box
+        console.log(this.storeInput.value);
+        //second we're going to transition
+        //from / to /store/:storeID
+        console.log('You changed the URL!');
+    }
+
     render(){
         return(
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
             <h2>Please enter a store</h2>
             <input type="text" required placeholder="Store Name"
-            defaultValue={getFunName()} />
-           <button type="submit">Visit Store</button>
+            defaultValue={getFunName()} ref={(input) => {this.storeInput = input}}/>
+           <button type="submit">Visit Store -> </button>
             </form>
         )
     }

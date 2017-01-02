2aac3f60920086fba757b360224832cf0b059682
Alison Stuart <alisonnicolestuart@gmail.com>
2017-01-02 12:38:42 -0500

    Added the first bits of firebase connectivity.

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,8 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyBhp-6M_HPEbWpsinJ3imo9pauRE4InZ3o",
+    authDomain: "catch-of-the-day-e160a.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-e160a.firebaseio.com",
+});
+export default base;
